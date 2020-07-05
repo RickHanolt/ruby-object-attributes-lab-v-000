705449810893331e59a03d1d3caea562ee655f07
Richard Hanolt <rhanolt@gmail.com>
2020-07-05 09:48:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,6 @@
 
 class Person
 
-  def name=(some_name)
-    @name = some_name
-  end
-
-  def name
-    @name
-  end
-
-  def job=(some_job)
-    @job = some_job
-  end
-
-  def job
-    @job
-  end
+attr_accessor :name, :job
 
 end
